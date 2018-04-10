0a184d14a97cbdb3361b670682143d943829ee12
mohamedmagdy123 <36212518+mohamedmagdy123@users.noreply.github.com>
2018-04-10 10:05:57 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,39 +1,25 @@
-// FCI – Programming 1 – 2018 - Assignment 2
-// Program Name: ceaser cipher.cpb
-// Last Modification Date: 1/3/2018
-// Author1 and ID and Group: Mohamed Magdy 20170251
-// Teaching Assistant: Eng.Omar
-// Purpose:encryption & decryption
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+using namespace std ;
 
-using namespace std;
+double Fact (double x)
+{
+if (x == 1)
+return 1;
+return (x * Fact(x-1));
+}
 
 int main()
 {
-    char message[100],ch;
-    int i,shift;
-    cout << "enter a message u want to encrypt!:";
-    cin.getline(message , 100);
-    cout << "how many letters u want to shift?:";
-    cin >> shift;
-    for(i = 0 ; message[i] != '\0' ; ++i){ // the '\0' means the end of the string(terminate the string)
-        ch = message[i];
+    double x , equation ,  sum = 0  ;
+    cout << "enter the value of x " << endl ;
+    cin >> x ;
+    for (int i = 2 ; i <= 100 ; i++)
+    {
+        equation = pow(x , i) / Fact(i) ;
+        sum += equation  ;
 
-        if(ch >= 'a' && ch <= 'z'){
-            ch = ch + shift;
-            if(ch > 'z' ){ // in ASCII
-                    ch = ch -'z' + 'a' - 1;// to take just letters
-            }
-            message[i] = ch;
-        }
-        else if(ch >= 'A' && ch <= 'Z'){
-            ch = ch + shift;
-            if(ch > 'Z'){
-                ch = ch - 'Z' + 'A' - 1;
-            }
-            message[i] = ch;
-        }
     }
-    cout << "your encrypted message is : " << message << endl;
-    return 0;
+    sum += 1+x ;
+    cout << "Result = " << sum  ;
 }
